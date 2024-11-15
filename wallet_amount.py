65755265acaae081705281506991087afa3214a7
Shm00py <willwalterduff@icloud.com>
2024-11-14 19:04:52 -0500

    Scraping transaction amount, checking if table columns already created

--- wallet_amount.py
+++ wallet_amount.py
@@ -24,12 +24,14 @@ async def get_wallet_total(addresses):
         
         received_div = parent_div[0].find('div', class_='sc-92d5245a-4 eacMWo')
         sent_div = parent_div[1].find('div', class_='sc-92d5245a-4 eacMWo')
+        transactions_div = parent_div[5].find('div', class_='sc-92d5245a-2 cWXKHx')
         
         received = received_div.text.strip().replace('$', '').replace(',', '')
         sent = sent_div.text.strip().replace('$', '').replace(',', '')
         crypto = 'Ethereum'
+        transactions = transactions_div.text.strip().replace(',', '')
         
-        wallet_data[address] = {'received': received, 'sent': sent, 'crypto': crypto}
+        wallet_data[address] = {'received': received, 'sent': sent,'crypto': crypto, 'transactions': transactions}
         continue
         
       except Exception as e:
@@ -46,16 +48,18 @@ async def get_wallet_total(addresses):
         
         received_div = parent_div[0].find('div', class_='sc-92d5245a-4 eacMWo')
         sent_div = parent_div[1].find('div', class_='sc-92d5245a-4 eacMWo')
+        transactions_div = parent_div[3].find('div', class_='sc-92d5245a-2 cWXKHx')
         
         received = received_div.text.strip().replace('$', '').replace(',', '')
-        sent = sent_div.text.strip().replace('$', '').replace(',', '')
+        sent = sent_div.text.strip().replace('$', '').replace(',', '')        
         crypto = 'Bitcoin'
+        transactions = transactions_div.text.strip().replace(',', '')
         
-        wallet_data[address] = {'received': received, 'sent': sent, 'crypto': crypto}
+        wallet_data[address] = {'received': received, 'sent': sent, 'crypto': crypto, 'transactions': transactions}
 
       except Exception as e:
         print("-- Invalid address")
-        wallet_data[address] = {'received': None, 'sent': None, 'crypto': None}
+        wallet_data[address] = {'received': None, 'sent': None, 'crypto': None, 'transactions': None}
         continue
         
     await browser.close()
@@ -73,20 +77,31 @@ async def get_wallets():
 
   wallet_data = await get_wallet_total(addresses)
   
-  cursor.execute('ALTER TABLE wallet_reports ADD COLUMN received NUMERIC;')
-  cursor.execute('ALTER TABLE wallet_reports ADD COLUMN sent NUMERIC;')
-  cursor.execute('ALTER TABLE wallet_reports ADD COLUMN crypto TEXT;')
+  columns_to_add = [
+    ("received", "NUMERIC"),
+    ("sent", "NUMERIC"),
+    ("crypto", "TEXT"),
+    ("transactions", "INTEGER")
+  ]
+
+  for column_name, column_type in columns_to_add:
+    try:
+      cursor.execute(f"ALTER TABLE wallet_reports ADD COLUMN {column_name} {column_type};")
+      print(f"Added column '{column_name}'")
+    except sqlite3.OperationalError:
+      print(f"Column '{column_name}' already exists")
   
   for address, data in wallet_data.items():
     received = data['received']
     sent = data['sent']
     crypto = data['crypto']
+    transactions = data['transactions']
 
     cursor.execute('''
       UPDATE wallet_reports
-      SET received = ?, sent = ?, crypto = ?
+      SET received = ?, sent = ?, crypto = ?, transactions = ?
       WHERE wallet = ?;
-    ''', (received, sent, crypto, address))
+    ''', (received, sent, crypto, transactions, address))
         
   conn.commit()
   conn.close()
